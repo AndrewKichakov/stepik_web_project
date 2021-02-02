b89df23de8b6930bcf671eecb58bcb3342d4d4c4
AndrewKichakov <ku4akov@gmail.com>
2021-02-02 15:49:19 +0300

    Add config and upgrade init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,7 @@
-sudo apt update
-sudo apt install python3.9
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.9 /usr/bin/python3
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade django==2.1
-sudo pip3 install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 ~/web/hello:app
-sudo gunicorn -b 0.0.0.0:8000 ~/web/ask.wsgi:application
+sudo gunicorn -b 0.0.0.0:8000 ~/web/ask/ask.wsgi:application
