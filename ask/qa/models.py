d391f085d392baa8a45afd67075204437af18d6a
AndrewKichakov <ku4akov@gmail.com>
2021-02-09 19:39:01 +0300

    Added database

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Question - вопрос
+# title - заголовок вопроса
+# text - полный текст вопроса
+# added_at - дата добавления вопроса
+# rating - рейтинг вопроса (число)
+# author - автор вопроса
+# likes - список пользователей, поставивших "лайк"
+
+# Answer - ответ
+# text - текст ответа
+# added_at - дата добавления ответа
+# question - вопрос, к которому относится ответ
+# author - автор ответа
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(default='', max_length=255)
+    text = models.TextField(default='')
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField(default='')
+    added_at = models.DateTimeField(null=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
+sudo apt update
+sudo apt install python3.5
+sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install -r requirements.txt
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 ~/web/hello:app
+# sudo gunicorn -b 0.0.0.0:8080 ~/web/hello:app
 sudo gunicorn -b 0.0.0.0:8000 ~/web/ask/ask.wsgi:application
