d5242d09f7d066531bd04d125cb202a5f9057cd2
AndrewKichakov <ku4akov@gmail.com>
2021-02-15 13:38:36 +0300

    Upgrade models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-# Question - вопрос
-# title - заголовок вопроса
-# text - полный текст вопроса
-# added_at - дата добавления вопроса
-# rating - рейтинг вопроса (число)
-# author - автор вопроса
-# likes - список пользователей, поставивших "лайк"
-
-# Answer - ответ
-# text - текст ответа
-# added_at - дата добавления ответа
-# question - вопрос, к которому относится ответ
-# author - автор ответа
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
