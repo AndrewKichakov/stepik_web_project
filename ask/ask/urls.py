2b08647a7877f3d7299b6fe0ca47834438d14f93
Andrew Kichakov <ku4akov@gmail.com>
2022-06-28 19:48:57 +0300

    2.5 v1

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from qa.views import test
+from qa.views import test, index, popular, question
 
 
 urlpatterns = [
-    path('', test),
+    path('', index, name='index'),
     path('login/', test, name='login'),
     path('signup/', test, name='signup'),
-    path('question/<int:id>/', test, name='question'),
+    path('question/<int:id>/', question, name='question'),
     path('ask/', test, name='ask'),
-    path('popular/', test, name='popular'),
-    path('new/', test, name='new')
+    path('popular/', popular, name='popular'),
+    path('new/', test, name='new'),
 ]
--- init.sh
+++ init.sh
@@ -1,17 +1,8 @@
 sudo apt update
+sudo apt install sqlite3
 sudo apt install python3.5
 sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
-# sudo pip3 install Django==2.2.24
 sudo pip3 install -r requirements.txt
-
-# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-# sudo nginx -t
-# sudo /etc/init.d/nginx restart
-
-# sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-# sudo /etc/init.d/gunicorn restart
-# sudo gunicorn -b 0.0.0.0:8080 ~/web/hello:app
-# sudo gunicorn -b 0.0.0.0:8000 ~/web/ask/ask.wsgi:application
-
 sudo /etc/init.d/nginx stop
-sudo python3 ~/web/ask/manage.py runserver 0.0.0.0:80
+sudo /etc/init.d/gunicorn stop
+sudo python3 ~/web/ask/manage.py runserver 0.0.0.0:80 &
