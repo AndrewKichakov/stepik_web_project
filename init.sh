6a2d6d513477a60f3a32f46ce1fe7e8f511f1767
Andrew <49459680+akichakov@users.noreply.github.com>
2022-06-28 20:10:14 +0300

    Update init.sh
    2.5 v3

--- init.sh
+++ init.sh
@@ -5,4 +5,6 @@ sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install -r requirements.txt
 sudo /etc/init.d/nginx stop
 sudo /etc/init.d/gunicorn stop
+sudo python3 ~/web/ask/manage.py makemigrations
+sudo python3 ~/web/ask/manage.py migrate
 sudo python3 ~/web/ask/manage.py runserver 0.0.0.0:80 &
