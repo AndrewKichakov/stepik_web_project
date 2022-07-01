3e64e976478c661f17dd25c869c23c6efdf92060
Andrew Kichakov <ku4akov@gmail.com>
2022-07-01 16:12:32 +0300

    2.5 upgrade

--- init.sh
+++ init.sh
@@ -1,11 +1,6 @@
-sudo apt update
-sudo apt install libmysqlclient-dev
-sudo apt install python3.5
-sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install -r requirements.txt
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/gunicorn stop
-# sudo python3 ~/web/ask/manage.py check
-# sudo python3 ~/web/ask/manage.py makemigrations
-# sudo python3 ~/web/ask/manage.py migrate
-# sudo python3 ~/web/ask/manage.py runserver 0.0.0.0:80 &
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
