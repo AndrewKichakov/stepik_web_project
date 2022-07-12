ef2860f5257cc37a60816ac091e3e41ac7ab776b
Andrew Kichakov <ku4akov@gmail.com>
2022-07-12 16:11:47 +0300

    Delete ask2

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def index(request):
+    return render(request, 'qa/index.html')
